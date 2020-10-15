3b22de0bab202ed89a8608c1a6a6cb31d1953954
hoangmanhtu <hoangmanhtu0809@gmail.com>
2020-10-15 13:34:02 +0700

    Revert "git push origin master"
    This reverts commit 0a9ea65d718a45b73501825c11df18eb193de03a.

--- Project/App/configs/Database.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Database {
-    const DB_DSN = "mysql:host=localhost;port=3306;dbname=do_anhttt;charset=utf8";
-    const DB_USERNAME = 'root';
-    const DB_PASSWORD = '';
-}
--- Project/nganhang/Database.php
+++
@@ -1,8 +0,0 @@
-<?php
-//configs/Database.php
-//class chứa các hằng số liên quan đến kết nối CSDL
-class Database {
-    const DB_DSN = "mysql:host=localhost;port=3306;dbname=mvc_demo;charset=utf8";
-    const DB_USERNAME = 'root';
-    const DB_PASSWORD = '';
-}
